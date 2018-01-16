031c4e27ce068b7f737361ca00d7dcff2a08b524
DeveloperNelsoft <nelsonjoseacosta@gmail.com>
2018-01-16 15:11:26 -0400

    Configurating initial options for animation.

---
+++ AnimatedList/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace AnimatedList
+{
+    [Activity(Label = "AnimatedList", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button mBtnGoToList;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mBtnGoToList = FindViewById<Button>(Resource.Id.btnGoToList);
+
+            mBtnGoToList.Click += MBtnGoToList_Click;
+        }
+
+        private void MBtnGoToList_Click(object sender, System.EventArgs e)
+        {
+            Intent animatedActivity = new Intent(this, typeof(AnimatedListActivity));
+            StartActivity(animatedActivity);
+        }
+    }
+}
+
